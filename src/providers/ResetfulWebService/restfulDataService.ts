0555501a6d2a24d41cd4430a27b931b99c6ac455
SachiraChin <scsachira@gmail.com>
2015-01-18 10:09:21 +0530

    restful data service added

---
+++ src/providers/ResetfulWebService/restfulDataService.ts
@@ -0,0 +1,22 @@
+﻿
+
+module AngularCloudDataConnector {
+    export class RestfulDataService implements IDataService {
+        _dataId: number;
+        tableNames: Array<string>;
+
+
+        public add(tableName: string, entity: any, onsuccess: (newEntity: any) => void, onerror: (error: string) => void): void {
+            
+        }
+        public update(tableName: string, entity: any, onsuccess: (newEntity: any) => void, onerror: (error: string) => void): void {
+            
+        }
+        get(updateCallback: (result: any) => void, lastSyncDates: { [tableName: string]: Date; }): void  {
+            
+        }
+        remove(tableName: string, entity: any, onsuccess: () => void, onerror: (error: string) => void): void {
+            
+        }
+    }
+} 

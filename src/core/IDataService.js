72b18bcd42d9122985c06b0c52c75bf77b92382a
SachiraChin <scsachira@gmail.com>
2015-01-21 11:07:22 +0530

    modified solution structure to see samples seperately added rest service provider structure added WebAPI sample project

--- src/core/IDataService.js
+++
@@ -1,3 +0,0 @@
-﻿/* Copyright (c) Microsoft Open Technologies, Inc.  All rights reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information. */ /// <reference path="../../lib/angularjs/angular.d.ts" />
-/// <reference path="../../lib/jquery/jquery.d.ts" />
-//# sourceMappingURL=IDataService.js.map
--- src/providers/ResetfulWebService/restfulDataService.ts
+++ src/providers/ResetfulWebService/restfulDataService.ts
@@ -1,10 +1,23 @@
 ﻿
 
+/// <reference path="../../../lib/angularjs/angular.d.ts" />
+/// <reference path="../../../lib/jquery/jquery.d.ts" />
+/// <reference path="../../../dist/angular-cdc.d.ts" />
+
+declare var restSvc;
+
 module AngularCloudDataConnector {
     export class RestfulDataService implements IDataService {
         _dataId: number;
         tableNames: Array<string>;
+        $: JQuery;
+        public initSource(accessPaths) {
 
+            //TODO: Implement OAuth
+            //restSvc.config.credentials = undefined;
+
+            this.tableNames = accessPaths;
+        }
 
         public add(tableName: string, entity: any, onsuccess: (newEntity: any) => void, onerror: (error: string) => void): void {
             
